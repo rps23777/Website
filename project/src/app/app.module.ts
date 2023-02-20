c63f53f534f38649025531b2f54d2596c0dda30b
rps23777 <rps23777@gmail.com>
2023-02-20 15:41:37 -0500

    create components secure

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecureComponent } from './secure/secure.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecureComponent
   ],
   imports: [
     BrowserModule,
